f22fedd292f2f4a45ebc2f4dceca49877f465abc
Thinh Hoang Gia <hoanggiathinh2002@gmail.com>
2026-04-02 21:19:44 +0200

    Update install-choco.ps1

--- Artifacts/choco/install-choco.ps1
+++ Artifacts/choco/install-choco.ps1
@@ -1,11 +1,3 @@
-Write-Host "Checking for pending reboots to prepare for SQL Server..."
-# We don't need complex logic here because 'rebootBehavior: always' 
-# in the JSON will force a reboot as soon as this script exits with 0.
-Write-Host "Rebooting machine to clear component-based servicing or file renames..."
-exit 0
-
-
-
 Write-Host "Checking if Chocolatey is installed..."
 
 if (Get-Command choco.exe -ErrorAction SilentlyContinue) {
