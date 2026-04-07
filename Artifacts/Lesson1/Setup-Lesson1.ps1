0ca9713ea067d2339671479f9809646726913d3f
Thinh Hoang Gia <hoanggiathinh2002@gmail.com>
2026-04-07 11:42:50 +0200

    Update Setup-Lesson1.ps1

--- Artifacts/Lesson1/Setup-Lesson1.ps1
+++ Artifacts/Lesson1/Setup-Lesson1.ps1
@@ -1,1 +1,12 @@
+param (
+    [string]$DBName = "TrainingDB"
+)
 
+# Use the local SQL instance (assuming it's installed on the VM)
+$ServerInstance = "localhost"
+
+# Execute the T-SQL script
+Invoke-Sqlcmd -ServerInstance $ServerInstance -Database "master" -Query "IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = '$DBName') CREATE DATABASE [$DBName];"
+Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $DBName -InputFile "./Tables-Schema.sql"
+
+Write-Host "Milestone 1 successfully applied to $DBName"
