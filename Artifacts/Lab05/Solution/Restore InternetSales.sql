140722af5412b1cbf77f8a9ce228714b85d1e28c
Thinh Hoang Gia <hoanggiathinh2002@gmail.com>
2026-04-07 17:22:07 +0200

    Create Restore InternetSales.sql

---
+++ Artifacts/Lab05/Solution/Restore InternetSales.sql
@@ -0,0 +1,11 @@
+USE master;
+BACKUP LOG InternetSales TO DISK = 'C:\Backups\IS-TailLog.bak'WITH NO_TRUNCATE;
+
+GO
+
+RESTORE DATABASE [InternetSales] FROM  DISK = N'C:\Backups\InternetSales.bak' WITH  FILE = 1,  NORECOVERY,  NOUNLOAD,  STATS = 5
+RESTORE DATABASE [InternetSales] FROM  DISK = N'C:\Backups\InternetSales.bak' WITH  FILE = 3,  NORECOVERY,  NOUNLOAD,  STATS = 5
+RESTORE LOG [InternetSales] FROM  DISK = N'C:\Backups\InternetSales.bak' WITH  FILE = 4,  NORECOVERY,  NOUNLOAD,  STATS = 5
+RESTORE LOG [InternetSales] FROM  DISK = N'C:\Backups\IS-TailLog.bak' WITH  FILE = 1,  NOUNLOAD,  STATS = 5
+
+GO
