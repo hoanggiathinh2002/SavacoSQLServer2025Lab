126eb0391de852e6f33e7cf8a93cdc27873cc128
Thinh Hoang Gia <hoanggiathinh2002@gmail.com>
2026-04-07 17:16:42 +0200

    Create Setup.cmd

---
+++ Artifacts/Lab05/Starter/Setup.cmd
@@ -0,0 +1,37 @@
+@Echo Off
+ECHO Preparing the lab environment...
+
+REM - Get current directory
+SET SUBDIR=%~dp0
+
+REM - Restart SQL Server Service
+NET STOP SQLSERVERAGENT
+NET STOP MSSQLSERVER
+NET STOP SQLAGENT$SQL2
+NET STOP MSSQL$SQL2
+NET START MSSQLSERVER
+NET START MSSQL$SQL2
+NET START SQLAGENT$SQL2
+
+REM Create folders for database files
+ECHO Creating folders for database files (ignore errors if they already exist!)...
+MD C:\Logs > NUL
+MD C:\Data > NUL
+MD C:\Backups > NUL
+ECHO Deleting backup files (ignore errors if they don't exist!)...
+DEL C:\Backups\*.* /Q > NUL
+
+REM - Run SQL Script to prepare the database environment
+ECHO Configuring databases...
+SQLCMD -S MIA-SQL\SQL2 -E -i %SUBDIR%SetupFiles\Setup2.sql > NUL
+SQLCMD -E -i %SUBDIR%SetupFiles\Setup.sql > NUL
+
+REM Sabotage AdventurWorks
+ECHO Sabotaging databases...
+NET STOP MSSQLSERVER
+DEL C:\Data\HumanResources.mdf /Q
+COPY %SUBDIR%SetupFiles\InternetSales.mdf.txt C:\Data\InternetSales.mdf /Y
+NET START MSSQLSERVER
+NET START SQLSERVERAGENT
+
+pause
