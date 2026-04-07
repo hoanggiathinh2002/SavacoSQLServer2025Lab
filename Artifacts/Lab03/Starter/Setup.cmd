939a4d367d462a337953ea452e5a3a085da10f5b
Thinh Hoang Gia <hoanggiathinh2002@gmail.com>
2026-04-07 17:07:40 +0200

    Add files via upload

---
+++ Artifacts/Lab03/Starter/Setup.cmd
@@ -0,0 +1,31 @@
+@Echo Off
+ECHO Preparing the lab environment...
+
+REM - Get current directory
+SET SUBDIR=%~dp0
+
+REM - Restart SQL Server Service
+NET STOP SQLSERVERAGENT
+NET STOP MSSQLSERVER
+NET STOP SQLAGENT$SQL2
+NET STOP MSSQL$SQL2
+NET START MSSQLSERVER
+NET START MSSQL$SQL2
+NET START SQLSERVERAGENT
+NET START SQLAGENT$SQL2
+
+REM - Run SQL Script to prepare the database environment
+ECHO Configuring databases...
+SQLCMD -S MIA-SQL\SQL2 -E -i %SUBDIR%SetupFiles\Setup2.sql > NUL
+SQLCMD -E -i %SUBDIR%SetupFiles\Setup.sql > NUL
+
+REM Create folders for database files
+ECHO Creating folders for database files
+MD C:\Logs > NUL
+MD C:\Data > NUL
+
+XCOPY %SUBDIR%SetupFiles\AWData*.* %SUBDIR% /Q /Y
+
+PAUSE
+
+
---
+++ Artifacts/Lab03/Starter/ViewFileInfo.sql
@@ -0,0 +1,29 @@
+-- View page usage
+USE InternetSales;
+GO
+
+SELECT f.name FileName, fg.name Filegroup, u.allocated_extent_page_count UsedPages, u.total_page_count TotalPages
+FROM sys.sysfiles f
+JOIN sys.filegroups fg ON f.groupid = fg.data_space_id
+JOIN sys.dm_db_file_space_usage u ON u.file_id = f.fileid;
+GO
+
+-- Create a table on the SalesData filegroup
+CREATE TABLE dbo.SalesOrder
+(
+	OrderID int IDENTITY PRIMARY KEY,
+	OrderDate datetime DEFAULT GETDATE()
+)
+ON SalesData;
+GO
+
+-- Insert 10,000 rows
+INSERT INTO SalesOrder VALUES (DEFAULT);
+GO 10000
+
+-- View page usage again
+SELECT f.name FileName, fg.name Filegroup, u.allocated_extent_page_count UsedPages, u.total_page_count TotalPages
+FROM sys.sysfiles f
+JOIN sys.filegroups fg ON f.groupid = fg.data_space_id
+JOIN sys.dm_db_file_space_usage u ON u.file_id = f.fileid;
+GO
