466e8e82eb7eb92e5f86895f504792d73aaf59c0
hoanggiathinh2002 <hoanggiathinh2002@gmail.com>
2026-04-09 15:07:03 +0200

    Update Setup.cmd

--- Artifacts/Lab04/Starter/Setup.cmd
+++ Artifacts/Lab04/Starter/Setup.cmd
@@ -7,12 +7,12 @@ SET SUBDIR=%~dp0
 REM - Restart SQL Server Service
 NET STOP SQLSERVERAGENT
 NET STOP MSSQLSERVER
-NET STOP SQLAGENT$SQL2
-NET STOP MSSQL$SQL2
+REM NET STOP SQLAGENT$SQL2
+REM NET STOP MSSQL$SQL2
 NET START MSSQLSERVER
-NET START MSSQL$SQL2
+REM NET START MSSQL$SQL2
 NET START SQLSERVERAGENT
-NET START SQLAGENT$SQL2
+REM NET START SQLAGENT$SQL2
 
 REM Create folders for database files
 ECHO Creating folders for database files (ignore errors if they already exist!)...
@@ -24,8 +24,7 @@ DEL C:\Backups\*.bak /Q > NUL
 
 REM - Run SQL Script to prepare the database environment
 ECHO Configuring databases...
-SQLCMD -S MIA-SQL\SQL2 -E -i %SUBDIR%SetupFiles\Setup2.sql > NUL
-SQLCMD -E -i %SUBDIR%SetupFiles\Setup.sql > NUL
+SQLCMD -S localhost -E -i %SUBDIR%SetupFiles\Setup2.sql -C > NUL
+SQLCMD -E -i %SUBDIR%SetupFiles\Setup.sql -C > NUL
 
-
-PAUSE
+ECHO Setup Complete.
