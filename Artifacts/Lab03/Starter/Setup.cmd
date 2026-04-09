b8e0835c46097e36d59dc4081f312a53552237f0
hoanggiathinh2002 <hoanggiathinh2002@gmail.com>
2026-04-09 11:43:33 +0200

    Update Setup.cmd

--- Artifacts/Lab03/Starter/Setup.cmd
+++ Artifacts/Lab03/Starter/Setup.cmd
@@ -7,17 +7,18 @@ SET SUBDIR=%~dp0
 REM - Restart SQL Server Service
 NET STOP SQLSERVERAGENT
 NET STOP MSSQLSERVER
-NET STOP SQLAGENT$SQL2
-NET STOP MSSQL$SQL2
+REM - NET STOP SQLAGENT$SQL2
+REM - NET STOP MSSQL$SQL2
+
 NET START MSSQLSERVER
-NET START MSSQL$SQL2
+REM - NET START MSSQL$SQL2
 NET START SQLSERVERAGENT
-NET START SQLAGENT$SQL2
+REM - NET START SQLAGENT$SQL2
 
 REM - Run SQL Script to prepare the database environment
 ECHO Configuring databases...
-SQLCMD -S localhost\SQL2 -E -i %SUBDIR%SetupFiles\Setup2.sql > NUL
-SQLCMD -E -i %SUBDIR%SetupFiles\Setup.sql > NUL
+SQLCMD -S localhost -E -i %SUBDIR%SetupFiles\Setup2.sql -C > NUL
+SQLCMD -E -i %SUBDIR%SetupFiles\Setup.sql -C > NUL
 
 REM Create folders for database files
 ECHO Creating folders for database files
@@ -27,5 +28,4 @@ MD C:\Data > NUL
 XCOPY %SUBDIR%SetupFiles\AWData*.* %SUBDIR% /Q /Y
 
 PAUSE
-
 
