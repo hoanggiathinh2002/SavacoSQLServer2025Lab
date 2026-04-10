4b36433cac1c63b66e7df3f490f985d410ee75db
hoanggiathinh2002 <hoanggiathinh2002@gmail.com>
2026-04-10 11:02:45 +0200

    Update Setup.cmd

--- Artifacts/Lab07/Starter/Setup.cmd
+++ Artifacts/Lab07/Starter/Setup.cmd
@@ -6,14 +6,14 @@ SET SUBDIR=%~dp0
 
 REM - Restart SQL Server Service to force closure of any open connections
 ECHO Restarting SQL Server services...
-NET STOP SQLSERVERAGENT
-NET STOP MSSQLSERVER
-NET STOP SQLAGENT$SQL2
-NET STOP MSSQL$SQL2
-NET START MSSQLSERVER
-NET START MSSQL$SQL2
-NET START SQLSERVERAGENT
-NET START SQLAGENT$SQL2
+NET STOP SQLSERVERAGENT 
+NET STOP MSSQLSERVER 
+REM NET STOP SQLAGENT$SQL2 
+REM NET STOP MSSQL$SQL2 
+NET START MSSQLSERVER 
+REM NET START MSSQL$SQL2 
+NET START SQLSERVERAGENT 
+REM NET START SQLAGENT$SQL2 
 
 REM Create folders for database files
 ECHO Creating folders for database files (ignore errors if they already exist!)...
@@ -23,8 +23,8 @@ MD C:\Backups > NUL
 
 REM - Run SQL Script to prepare the database environment
 ECHO Configuring databases...
-SQLCMD -S .\SQL2 -E -i %SUBDIR%SetupFiles\Setup2.sql
-SQLCMD -E -i %SUBDIR%SetupFiles\Setup.sql
+SQLCMD -S . -E -i %SUBDIR%SetupFiles\Setup2.sql -C
+SQLCMD -E -i %SUBDIR%SetupFiles\Setup.sql -C
 
 COPY %SUBDIR%SetupFiles\Baseline.ps1.txt %SUBDIR%Baseline.ps1 /Y
 COPY %SUBDIR%SetupFiles\Workload.ps1.txt %SUBDIR%Workload.ps1 /Y
@@ -34,7 +34,7 @@ DEL %SUBDIR%*.tif /Q
 RMDIR %SUBDIR%Logs /S /Q
 MKDIR %SUBDIR%Logs
 
-PAUSE
+ECHO Setup Complete. 
 
 
 
