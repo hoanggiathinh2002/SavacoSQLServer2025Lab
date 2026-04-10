76eb662284b3d37c813f3e4d58c11324e06216da
hoanggiathinh2002 <hoanggiathinh2002@gmail.com>
2026-04-10 12:07:02 +0200

    Update Setup.cmd

--- Artifacts/Lab11/Starter/Setup.cmd
+++ Artifacts/Lab11/Starter/Setup.cmd
@@ -5,14 +5,14 @@ REM - Get current directory
 SET SUBDIR=%~dp0
 
 REM - Restart SQL Server Service to force closure of any open connections
-NET STOP SQLSERVERAGENT
-NET STOP MSSQLSERVER
-NET STOP SQLAGENT$SQL2
-NET STOP MSSQL$SQL2
-NET START MSSQLSERVER
-NET START MSSQL$SQL2
-NET START SQLSERVERAGENT
-NET START SQLAGENT$SQL2
+NET STOP SQLSERVERAGENT 
+NET STOP MSSQLSERVER 
+REM NET STOP SQLAGENT$SQL2 
+REM NET STOP MSSQL$SQL2 
+NET START MSSQLSERVER 
+REM NET START MSSQL$SQL2 
+NET START SQLSERVERAGENT 
+REM NET START SQLAGENT$SQL2
 
 REM Create folders for database files
 ECHO Creating folders for database files (ignore errors if they already exist!)...
@@ -22,12 +22,12 @@ MD C:\Backups > NUL
 
 REM - Run SQL Script to prepare the database environment
 ECHO Preparing Databases...
-SQLCMD -S MIA-SQL\SQL2 -E -i %SUBDIR%SetupFiles\Setup2.sql > NUL
-SQLCMD -E -i %SUBDIR%SetupFiles\Setup.sql > NUL
+SQLCMD -S localhost -E -i %SUBDIR%SetupFiles\Setup2.sql -C > NUL 
+SQLCMD -E -i %SUBDIR%SetupFiles\Setup.sql -C > NUL  
 
 DEL %SUBDIR%*.txt /Q
 
-PAUSE
+ECHO Setup Complete.
 
 
 
