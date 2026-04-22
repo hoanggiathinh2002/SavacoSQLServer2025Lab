259eea869ce17a620614c27c9da4105bbaa9a535
hoanggiathinh2002 <hoanggiathinh2002@gmail.com>
2026-04-22 16:46:02 +0200

    Update Setup.cmd

--- Artifacts/Lab06/Starter/Setup.cmd
+++ Artifacts/Lab06/Starter/Setup.cmd
@@ -4,44 +4,33 @@ ECHO Preparing the lab environment...
 REM - Get current directory
 SET SUBDIR=%~dp0
 
-REM - Restart SQL Server Service to force closure of any open connections
+REM - Restart SQL Server Service
 ECHO Restarting SQL Server services...
 NET STOP SQLSERVERAGENT
 NET STOP MSSQLSERVER
-REM NET STOP SQLAGENT$SQL2
-REM NET STOP MSSQL$SQL2
 NET START MSSQLSERVER
-REM NET START MSSQL$SQL2
 NET START SQLSERVERAGENT
-REM NET START SQLAGENT$SQL2
 
 REM Create folders for database files
-ECHO Creating folders for database files (ignore errors if they already exist!)...
-MD C:\Logs > NUL
-MD C:\Data > NUL
-MD C:\Backups > NUL
-ECHO Deleting files (ignore errors if they don't exist!)...
-DEL C:\Backups\*.bak /Q > NUL
-DEL %SUBDIR%*.xl* /Q
-DEL %SUBDIR%*.xml /Q
-DEL %SUBDIR%*.bacpac /Q
-COPY %SUBDIR%SetupFiles\CurrencyRates.csv C:\CurrencyRates.csv /Y
+ECHO Creating folders for database files...
+IF NOT EXIST "C:\Logs" MD C:\Logs
+IF NOT EXIST "C:\Data" MD C:\Data
+IF NOT EXIST "C:\Backups" MD C:\Backups
+
+ECHO Cleaning up old files (Keeping files from today)...
+/D -1 targets files modified before today. 
+REM Adjust -1 to -7 if you want to keep a week's worth.
+
+FORFILES /P "C:\Backups" /M *.bak /D -1 /C "cmd /c del @path" 2>NUL
+FORFILES /P "%SUBDIR%." /M *.xl* /D -1 /C "cmd /c del @path" 2>NUL
+FORFILES /P "%SUBDIR%." /M *.xml /D -1 /C "cmd /c del @path" 2>NUL
+FORFILES /P "%SUBDIR%." /M *.bacpac /D -1 /C "cmd /c del @path" 2>NUL
+
+COPY "%SUBDIR%SetupFiles\CurrencyRates.csv" "C:\CurrencyRates.csv" /Y
 
 REM - Run SQL Script to prepare the database environment
 ECHO Configuring databases...
-SQLCMD -S localhost -E -i %SUBDIR%SetupFiles\Setup2.sql -C > NUL 
+SQLCMD -S localhost -E -i "%SUBDIR%SetupFiles\Setup2.sql" -C > NUL 
+SQLCMD -E -i "%SUBDIR%SetupFiles\Setup.sql" -C > NUL 
 
-SQLCMD -E -i %SUBDIR%SetupFiles\Setup.sql -C > NUL 
-
-ECHO Setup Complete. 
-
-
-
-
-
-
-
-
-
-
-
+ECHO Setup Complete.
