ed96e61e24e9a9d0052a7130efe0d24c03fbee79
hoanggiathinh2002 <hoanggiathinh2002@gmail.com>
2026-04-23 13:53:10 +0200

    Update CreateUsers.sql

--- Artifacts/Lab09/Solution/CreateUsers.sql
+++ Artifacts/Lab09/Solution/CreateUsers.sql
@@ -7,18 +7,18 @@ FOR LOGIN Marketing_Application
 WITH DEFAULT_SCHEMA = Customers;
 
 CREATE USER WebApplicationSvc
-FOR LOGIN [ADVENTUREWORKS\WebApplicationSvc]
+FOR LOGIN [SQLServerSSMS\WebApplicationSvc]
 WITH DEFAULT_SCHEMA = Sales;
 
 CREATE USER InternetSales_Users
-FOR LOGIN [ADVENTUREWORKS\InternetSales_Users]
+FOR LOGIN [SQLServerSSMS\InternetSales_Users]
 WITH DEFAULT_SCHEMA = Sales;
 
 CREATE USER InternetSales_Managers
-FOR LOGIN [ADVENTUREWORKS\InternetSales_Managers]
+FOR LOGIN [SQLServerSSMS\InternetSales_Managers]
 WITH DEFAULT_SCHEMA = Sales;
 
 CREATE USER Database_Managers
-FOR LOGIN [ADVENTUREWORKS\Database_Managers]
+FOR LOGIN [SQLServerSSMS\Database_Managers]
 WITH DEFAULT_SCHEMA = dbo;
 
