128017cc7e8281259e24f5de01716a2850ed36d1
hoanggiathinh2002 <hoanggiathinh2002@gmail.com>
2026-04-23 09:43:44 +0200

    Update Setup.cmd

--- Artifacts/Lab06/Starter/Setup.cmd
+++ Artifacts/Lab06/Starter/Setup.cmd
@@ -12,25 +12,21 @@ NET START MSSQLSERVER
 NET START SQLSERVERAGENT
 
 REM Create folders for database files
-ECHO Creating folders for database files...
-IF NOT EXIST "C:\Logs" MD C:\Logs
-IF NOT EXIST "C:\Data" MD C:\Data
-IF NOT EXIST "C:\Backups" MD C:\Backups
-
-ECHO Cleaning up old files (Keeping files from today)...
-/D -1 targets files modified before today. 
-REM Adjust -1 to -7 if you want to keep a week's worth.
-
-FORFILES /P "C:\Backups" /M *.bak /D -1 /C "cmd /c del @path" 2>NUL
-FORFILES /P "%SUBDIR%." /M *.xl* /D -1 /C "cmd /c del @path" 2>NUL
-FORFILES /P "%SUBDIR%." /M *.xml /D -1 /C "cmd /c del @path" 2>NUL
-FORFILES /P "%SUBDIR%." /M *.bacpac /D -1 /C "cmd /c del @path" 2>NUL
-
-COPY "%SUBDIR%SetupFiles\CurrencyRates.csv" "C:\CurrencyRates.csv" /Y
+ECHO Creating folders for database files (ignore errors if they already exist!)...
+MD C:\Logs > NUL
+MD C:\Data > NUL
+MD C:\Backups > NUL
+ECHO Deleting files (ignore errors if they don't exist!)...
+DEL C:\Backups\*.bak /Q > NUL
+DEL %SUBDIR%*.xl* /Q
+DEL %SUBDIR%*.xml /Q
+DEL %SUBDIR%*.bacpac /Q
+COPY %SUBDIR%SetupFiles\CurrencyRates.csv C:\CurrencyRates.csv /Y
 
 REM - Run SQL Script to prepare the database environment
 ECHO Configuring databases...
-SQLCMD -S localhost -E -i "%SUBDIR%SetupFiles\Setup2.sql" -C > NUL 
-SQLCMD -E -i "%SUBDIR%SetupFiles\Setup.sql" -C > NUL 
+SQLCMD -S localhost -E -i %SUBDIR%SetupFiles\Setup2.sql -C > NUL 
 
-ECHO Setup Complete.
+SQLCMD -E -i %SUBDIR%SetupFiles\Setup.sql -C > NUL 
+
+ECHO Setup Complete. 
